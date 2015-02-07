f96456e6e877b6c1e5f1cd811229517b39ab9160
ding wei <davidinsuomi@gmail.com>
2015-02-07 16:58:33 +0200

    create bpel in  the code

--- app/src/main/java/workflow/ee/ut/workflow/WorkFlowGenerate.java
+++ app/src/main/java/workflow/ee/ut/workflow/WorkFlowGenerate.java
@@ -8,6 +8,7 @@ import org.xmlpull.v1.XmlSerializer;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -44,6 +45,113 @@ public class WorkFlowGenerate {
         activityMap = workflowProcess.activityMap;
         variables = workflowProcess.variables;
         partnerLinks = workflowProcess.partnerLinks;
+    }
+
+    public static WorkFlowGenerate testWorkFlowInstance(){
+        WorkFlowProcess process = new WorkFlowProcess();
+        ArrayList<PartnerLink> partnerLinks = new ArrayList<PartnerLink>();
+        ArrayList<WorkFlowVariable> variables = new ArrayList<WorkFlowVariable>();
+        Map<String,ArrayList<String>> graphMap = new HashMap<String,ArrayList<String>>();
+        Map<String,ArrayList<String>> graphMapBackword = new HashMap<String,ArrayList<String>>();
+        Map<String,WorkFlowActivity> activityMap = new HashMap<String,WorkFlowActivity>();
+        // logic to fill all the variable
+
+        WorkFlowVariable variable1 = new WorkFlowVariable("wsdlResponse1","tns:GetFileResponseMessage");
+        WorkFlowVariable variable2 = new WorkFlowVariable("wsdlResponse2","tns:GetFileResponseMessage");
+        WorkFlowVariable variable3 = new WorkFlowVariable("wsdlResponse3","tns:GetFileResponseMessage");
+        WorkFlowVariable variable4 = new WorkFlowVariable("wsdlResponse4","tns:GetFileResponseMessage");
+        WorkFlowVariable variable5 = new WorkFlowVariable("wsdlResponse5","tns:GetFileResponseMessage");
+        WorkFlowVariable variable6 = new WorkFlowVariable("WSDLProcessorResponse1","tns:GetFileResponseMessage");
+        WorkFlowVariable variable7 = new WorkFlowVariable("WSDLProcessorResponse2","tns:GetFileResponseMessage");
+        WorkFlowVariable variable8 = new WorkFlowVariable("WSDLProcessorResponse3","tns:GetFileResponseMessage");
+        WorkFlowVariable variable9 = new WorkFlowVariable("WSDLProcessorResponse4","tns:GetFileResponseMessage");
+        WorkFlowVariable variable10 = new WorkFlowVariable("WSDLProcessorResponse5","tns:GetFileResponseMessage");
+        variables.add(variable1);
+        variables.add(variable2);
+        variables.add(variable3);
+        variables.add(variable4);
+        variables.add(variable5);
+        variables.add(variable6);
+        variables.add(variable7);
+        variables.add(variable8);
+        variables.add(variable9);
+        variables.add(variable10);
+
+        PartnerLink partnerLink1 = new PartnerLink("getData1PL","tns:GetData",null,"http://192.168.1.1");
+        PartnerLink partnerLink2 = new PartnerLink("getData2PL","tns:GetData",null,"http://192.168.1.1");
+        PartnerLink partnerLink3 = new PartnerLink("getData3PL","tns:GetData",null,"http://192.168.1.1");
+        PartnerLink partnerLink4 = new PartnerLink("getData4PL","tns:GetData",null,"http://192.168.1.1");
+        PartnerLink partnerLink5 = new PartnerLink("getData5PL","tns:GetData",null,"http://192.168.1.1");
+        PartnerLink partnerLink6 = new PartnerLink("WSDLProcessor","tns:GetData",null,"http://www.wsdlprocessor.com");
+        partnerLinks.add(partnerLink1);
+        partnerLinks.add(partnerLink2);
+        partnerLinks.add(partnerLink3);
+        partnerLinks.add(partnerLink4);
+        partnerLinks.add(partnerLink5);
+        partnerLinks.add(partnerLink6);
+
+        WorkFlowInvoke getData1 = new WorkFlowInvoke("getData1","getData1PL",null,"","wsdlResponse1");
+        WorkFlowInvoke postData1 = new WorkFlowInvoke("postData1","WSDLProcessor",null,"wsdlResponse1","WSDLProcessorResponse1");
+        WorkFlowInvoke getData2 = new WorkFlowInvoke("getData2","getData2PL",null,"","wsdlResponse2");
+        WorkFlowInvoke postData2 = new WorkFlowInvoke("postData2","WSDLProcessor",null,"wsdlResponse2","WSDLProcessorResponse2");
+        WorkFlowInvoke getData3 = new WorkFlowInvoke("getData3","getData3PL",null,"","wsdlResponse3");
+        WorkFlowInvoke postData3 = new WorkFlowInvoke("postData3","WSDLProcessor",null,"wsdlResponse3","WSDLProcessorResponse3");
+        WorkFlowInvoke getData4 = new WorkFlowInvoke("getData4","getData4PL",null,"","wsdlResponse4");
+        WorkFlowInvoke postData4 = new WorkFlowInvoke("postData4","WSDLProcessor",null,"wsdlResponse4","WSDLProcessorResponse4");
+        WorkFlowInvoke getData5 = new WorkFlowInvoke("getData5","getData5PL",null,"","wsdlResponse5");
+        WorkFlowInvoke postData5 = new WorkFlowInvoke("postData5","WSDLProcessor",null,"wsdlResponse5","WSDLProcessorResponse5");
+        WorkFlowInvoke enterPoint = new WorkFlowInvoke("enterPoint","",null,"","");
+        WorkFlowInvoke endPoint = new WorkFlowInvoke("endPoint","",null,"","");
+        activityMap.put("enterPoint",enterPoint);
+        activityMap.put("endPoint",endPoint);
+        activityMap.put("getData1",getData1);
+        activityMap.put("postData1",postData1);
+        activityMap.put("getData2",getData2);
+        activityMap.put("postData2",postData2);
+        activityMap.put("getData3",getData3);
+        activityMap.put("postData3",postData3);
+        activityMap.put("getData4",getData4);
+        activityMap.put("postData4",postData4);
+        activityMap.put("getData5",getData5);
+        activityMap.put("postData5",postData5);
+
+        graphMap.put("Beginnering", new ArrayList<String>(Arrays.asList("enterPoint")));
+        graphMap.put("enterPoint", new ArrayList<String>(Arrays.asList("getData1","getData2","getData3","getData4","getData5")));
+        graphMap.put("getData1", new ArrayList<String>(Arrays.asList("postData1")));
+        graphMap.put("getData2", new ArrayList<String>(Arrays.asList("postData2")));
+        graphMap.put("getData3", new ArrayList<String>(Arrays.asList("postData3")));
+        graphMap.put("getData4", new ArrayList<String>(Arrays.asList("postData4")));
+        graphMap.put("getData5", new ArrayList<String>(Arrays.asList("postData5")));
+
+        graphMap.put("postData1", new ArrayList<String>(Arrays.asList("endPoint")));
+        graphMap.put("postData2", new ArrayList<String>(Arrays.asList("endPoint")));
+        graphMap.put("postData3", new ArrayList<String>(Arrays.asList("endPoint")));
+        graphMap.put("postData4", new ArrayList<String>(Arrays.asList("endPoint")));
+        graphMap.put("postData5", new ArrayList<String>(Arrays.asList("endPoint")));
+
+        graphMap.put("endPoint", new ArrayList<String>(Arrays.asList("ending")));
+
+        graphMapBackword.put("enterPoint", new ArrayList<String>(Arrays.asList("Beginnering")));
+        graphMapBackword.put("getData1", new ArrayList<String>(Arrays.asList("enterPoint")));
+        graphMapBackword.put("getData2", new ArrayList<String>(Arrays.asList("enterPoint")));
+        graphMapBackword.put("getData3", new ArrayList<String>(Arrays.asList("enterPoint")));
+        graphMapBackword.put("getData4", new ArrayList<String>(Arrays.asList("enterPoint")));
+        graphMapBackword.put("getData5", new ArrayList<String>(Arrays.asList("enterPoint")));
+        graphMapBackword.put("postData1", new ArrayList<String>(Arrays.asList("getData1")));
+        graphMapBackword.put("postData2", new ArrayList<String>(Arrays.asList("getData2")));
+        graphMapBackword.put("postData3", new ArrayList<String>(Arrays.asList("getData3")));
+        graphMapBackword.put("postData4", new ArrayList<String>(Arrays.asList("getData4")));
+        graphMapBackword.put("postData5", new ArrayList<String>(Arrays.asList("getData5")));
+        graphMapBackword.put("endPoint", new ArrayList<String>(Arrays.asList("postData1","postData2","postData3","postData4","postData5")));
+        graphMapBackword.put("ending", new ArrayList<String>(Arrays.asList("endPoint")));
+
+        process.activityMap = activityMap;
+        process.graphMap = graphMap;
+        process.graphMapBackword = graphMapBackword;
+        process.variables = variables;
+        process.partnerLinks = partnerLinks;
+        WorkFlowGenerate generate = new WorkFlowGenerate(process);
+        return generate;
     }
     private void InitializeXmlSerializer() throws IllegalArgumentException, IllegalStateException, IOException {
         xmlSerializer.setOutput(writer);
